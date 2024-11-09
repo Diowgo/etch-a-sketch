8bbb068ab24bfae7fbc7df0b438f86b2c4d324e6
Diowgo <Diogovds71@gmail.com>
2024-11-09 18:33:07 -0300

    implementing grid size definition and squares responsivity

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 const squares = document.querySelector("div.squares");
+const gridSizeButton = document.querySelector("button.gridSize");
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement("div");
-        div.classList.add("row-" + i);
-        div.classList.add("column-" + j);
-        squares.appendChild(div);
-    }
+function generateGrid(size) {
+    for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                const div = document.createElement("div");
+                div.classList.add("row-" + i);
+                div.classList.add("column-" + j);
+
+                const heightAndWidth = returnWidthAndHeight(size);
+                setWidthAndHeight(div, heightAndWidth);
+
+                squares.appendChild(div);
+            }
+        }
 }
+
+function deleteGrid() {
+    squares.innerHTML = "";
+}
+
+function setWidthAndHeight(element, value) {
+    element.style.width = value;
+    element.style.height = value;
+}
+
+function returnWidthAndHeight(size) {
+    return 1000/size + "px";
+}
+
 
 
 function changeBackgroundColor(element) {
@@ -16,5 +37,12 @@ function changeBackgroundColor(element) {
 
 squares.addEventListener("mouseover", (event) => {
     const element = event.target;
-    changeBackgroundColor(element);
+    if (element.className != "squares") {
+        changeBackgroundColor(element);
+    }
+})
+
+gridSizeButton.addEventListener("click", (event) => {
+    deleteGrid();
+    generateGrid(parseInt(prompt("Grid size")));
 })
